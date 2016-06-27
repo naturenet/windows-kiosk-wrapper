02163db8e969f0db76e513b7a3612328d658b229
James B <jsoftwareengineering@users.noreply.github.com>
2016-06-27 17:53:37 -0400

    initial commit

---
+++ NatureNet/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace NatureNet
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //if the website changes addresses, simply change this line
+            NatureNetWV.Navigate(new Uri("http://nature-net.org"));
+        }
+
+        //when links are clicked that open a new window, no action is taken (as opposed to opening an new window
+        //which causes crash in assigned access mode)
+        private void NatureNetWV_NewWindowRequested(WebView sender, WebViewNewWindowRequestedEventArgs args)
+        {
+            args.Handled = true;
+        }
+    }
+}
